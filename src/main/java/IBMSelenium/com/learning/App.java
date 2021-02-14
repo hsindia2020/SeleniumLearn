f728a4a2b47afa114380128d1fdf3d4c427c6b63
Harmeet <hsindia2020@gmail.com>
2021-02-14 10:37:48 +0530

    SeleniumLearn

---
+++ src/main/java/IBMSelenium/com/learning/App.java
@@ -0,0 +1,13 @@
+package IBMSelenium.com.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/IBMSelenium/com/learning/AppTest.java
@@ -0,0 +1,35 @@
+package IBMSelenium.com.learning;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ *
+ * @author Harmeet Basic WebDriver driver.*
+ */
+
+public class AppTest
+{
+	public static void main(String args[]) {
+
+		WebDriverManager.firefoxdriver().setup();
+
+		WebDriver driver = new FirefoxDriver();
+
+		driver.get("https://www.google.com");
+
+		System.out.println(driver.getTitle());
+
+		System.out.println(driver.getCurrentUrl());
+
+		driver.get("https://yahoo.com");
+		driver.navigate().back();
+		driver.navigate().forward();
+
+		driver.close();// close current Browser
+
+//		driver.quit();// Quit will quit the driver
+	}
+}
